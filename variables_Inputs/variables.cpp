38f30a4e0a5db5f8c913bc2d77db228c87f4d675
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-11-11 19:01:14 +0530

    :zap: Variable inputs new

--- variables_Inputs/variables.cpp
+++ variables_Inputs/variables.cpp
@@ -2,6 +2,8 @@
 #include<string>
 int main(){
     int age;
+    long lng = 100000000000;
+    long long llng = 1000000000000;
     float num;
     char c = 'a';
     std::string name;
@@ -10,8 +12,11 @@ int main(){
     std::cout<< "Enter your age" << std::endl;
     std::cin>> age;
     std::cout<<"Your name is " << name << " And your are " << age << " years old"<<std::endl;
-    num = age;
-    std::cout<< c;
-    std::cout<< num;
+    num = age/2.0;
+    double dnum = 100.000;
+    std::cout<< c << std::endl;
+    std::cout<< lng<< std::endl;
+    std::cout<< dnum << std::endl;
+    std::cout<< num << std::endl;
     return 0;
 }
